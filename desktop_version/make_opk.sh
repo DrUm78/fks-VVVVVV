e74c6e6bb6deb6cb2ba43d93075f5d726768c241
Jack <jack@pixbits.com>
2021-03-09 19:17:47 +0100

    changed data.zip path to be in same folder as user data, fixed make_opk script

--- desktop_version/make_opk.sh
+++ desktop_version/make_opk.sh
@@ -5,7 +5,7 @@ OPK_NAME=vvvvvv.opk
 echo ${OPK_NAME}
 
 # create default.gcw0.desktop
-cat > default.gcw0.desktop <<EOF
+cat > default.funkey-s.desktop <<EOF
 [Desktop Entry]
 Name=VVVVVV
 Comment=2D puzzle platformer created by Terry Cavanagh
@@ -19,11 +19,11 @@ EOF
 
 # create opk
 FLIST="build/vvvvvv.x86"
-FLIST="${FLIST} default.gcw0.desktop"
+FLIST="${FLIST} default.funkey-s.desktop"
 FLIST="${FLIST} vvvvvv.png"
 
 rm -f ${OPK_NAME}
 mksquashfs ${FLIST} ${OPK_NAME} -all-root -no-xattrs -noappend -no-exports
 
-cat default.gcw0.desktop
-rm -f default.gcw0.desktop
+cat default.funkey-s.desktop
+rm -f default.funkey-s.desktop
